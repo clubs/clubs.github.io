0af707e2ea5578887fd0343579baf69faa540702
Margarita Grinvald <margaritagrinvald@gmail.com>
2018-07-27 12:59:04 +0200

    Replace thumbnails with gifs and add js

---
+++ main.js
@@ -0,0 +1,22 @@
+var boxObjectsDict = { 0: [1, 2, 3], 1: [1, 2, 3]};
+
+function popup(id) {
+  boxId = parseInt(id.slice(4));
+  var boxHtml = buildBoxHtml(boxId)
+  $('#boxPopupBody').html(boxHtml);
+  $('#boxPopup').modal();
+}
+
+function buildBoxHtml(boxId) {
+  var html = '';
+  objects = boxObjectsDict[boxId];
+  objects.forEach((objectId, index) => {
+    html += buildObjectHtml(objectId)
+  });
+  return html;
+}
+
+function buildObjectHtml(objectId) {
+  var html = objectId
+  return html
+}

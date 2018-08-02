809fd406048f7b568980072db83144969215baee
Margarita Grinvald <margaritagrinvald@gmail.com>
2018-08-02 16:27:30 +0200

    Testing json loading

--- js/main.js
+++ js/main.js
@@ -3,8 +3,9 @@ function object(id, src) {
     this.src = src;
 }
 
-$.getJSON('', function(data) {
+$.getJSON('/data/data.json', function(data) {
     //data is the JSON string
+    console.log(data)
 });
 
 var boxObjectsDict = { 0: [1, 2, 3], 1: [1, 2, 3]};

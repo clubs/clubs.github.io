d5bd6b85ee5022c0fb91e359a6f909beaff61eed
Margarita Grinvald <margaritagrinvald@gmail.com>
2018-08-03 17:40:44 +0200

    More boxes

--- js/main.js
+++ js/main.js
@@ -18,17 +18,30 @@ $.getJSON('data/objects.json', function(data) {
 
 function popup(id) {
   boxId = parseInt(id.slice(4));
-  // boxId = 4;
   var boxHtml = buildBoxHtml(boxId)
   $('#boxPopupBody').html(boxHtml);
   $('#boxPopupTitle').html('Box ' + boxId);
   $('#boxPopup').modal();
 }
 
+function removeDuplicates(arr){
+    let unique_array = []
+    for(let i = 0;i < arr.length; i++){
+        if(unique_array.indexOf(arr[i]) == -1){
+            unique_array.push(arr[i])
+        } else {
+          console.log(arr[i])
+        }
+    }
+    return unique_array
+}
+
 function buildBoxHtml(boxId) {
   var html = '';
 
   boxObjects = boxes[boxId].objects;
+  console.log(boxId);
+  boxObjects = removeDuplicates(boxObjects)
   boxObjects.forEach((objectId, index) => {
     html += buildObjectHtml(objectId)
   });
@@ -36,7 +49,6 @@ function buildBoxHtml(boxId) {
 }
 
 function buildObjectHtml(objectId) {
-  console.log(objects);
   var html = '<div class="row">'
   html += '<div class="col-6 mb-3"><img  class="img-fluid img-thumbnail" src="img/objects/' + objects[objectId].filename + '"></div>';
   html += '<div class="col-6">' + objects[objectId].filename + '</div>';
